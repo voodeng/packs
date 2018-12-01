c3ff2e6246494bb5f4e64f1d7304dd529c629aa0
Voodeng <voodeng@gmail.com>
2018-12-02 07:24:08 +0800

    feat(upversion): upversion can replace log file header

--- packages/archive/version.js
+++ packages/archive/version.js
@@ -13,6 +13,7 @@
  */
 
 const fs = require('fs')
+const fse = require('fs-extra')
 const path = require('path')
 var shell = require('shelljs')
 const argv = require('yargs')
@@ -21,15 +22,24 @@ const argv = require('yargs')
   .alias('h', 'help')
   .alias('v', 'version')
   .option('infile', {
-    describe: 'Changelog.md file path'
+    describe: 'Changelog.md file pathname. `./ptah/filename`',
   })
   .option('noskip', {
-    describe: 'all standard version default flow'
+    describe: 'all standard version default flow.',
   })
   .option('release-as', {
     describe:
-      'Package.json version bump type. `| major | minor | patch | premajor | preminor | prepatch | prerelease [--preid=<prerelease-id>] | from-git`'
+      'Package.json version bump type. `| major | minor | patch | premajor | preminor | prepatch | prerelease [--preid=<prerelease-id>] | from-git`',
+  })
+  .option('cpto', {
+    describe: 'Copy to target file pathname. `./ptah/filename`',
+  })
+  .option('header', {
+    describe: 'Replace log file header. `--header "# HEAD STRING\\n\\n descstring some"`',
   }).argv
+
+const root = process.cwd()
+const approot = path.resolve(__dirname).split('/node_modules')[0]
 
 // https://github.com/conventional-changelog/standard-version
 const standardVersion = require('standard-version')
@@ -40,6 +50,10 @@ const conventionalChangelog = require('conventional-changelog')
 
 // file path and name
 let LOGFILE = cmdParser.argv['infile'] || 'CHANGELOG.md' // log 文档位置
+let COPYTO = cmdParser.argv['cpto'] || ''
+let HEADER =
+  argv['header'] ||
+  '# Change Log\n\nChangelog file generate by @voodeng/archive `upversion` use [standard-version]'
 
 // parse GIT repo
 const gitUrl = shell
@@ -54,8 +68,13 @@ let REPO_NAME = G.name
 let GIT_REPO = `${REPO_SOURCE}/${REMO_OWNER}/${REPO_NAME}/` // git repo url
 let DOC_REPO = GIT_REPO // logfile link git url
 
+// replace coding
 if (REPO_SOURCE === 'git.coding.net') {
   DOC_REPO = `coding.net/u/${REMO_OWNER}/p/${REPO_NAME}/git/`
+}
+// replace tencent
+if (REPO_SOURCE === 'git.dev.tencent.com') {
+  DOC_REPO = `dev.tencent.com/u/${REMO_OWNER}/p/${REPO_NAME}/git/`
 }
 
 console.log(GIT_REPO, '====>', DOC_REPO)
@@ -86,7 +105,7 @@ const createLog = function(filename) {
       infile: filename,
       outfile: filename,
       releaseCount: 0,
-      sameFile: true
+      sameFile: true,
     },
     ''
   ).on('error', function(err) {
@@ -107,11 +126,48 @@ var jsonCMD = `node ${jsonCli} -I -f package.json -e "this.updateTime='${timesta
 var replaceCli = require.resolve('replace/bin/replace')
 var replaceCMD = `node ${replaceCli} ${GIT_REPO} ${DOC_REPO} ${LOGFILE}`
 
+// copy to
+var copyFile = (file, copyto) => {
+  if (!file || !copyto) return
+  fse
+    .copy(file, copyto)
+    .then(() => console.log('copy success!', file, '===>', copyto))
+    .catch(err => console.error(err))
+}
+
+// replace file, the line 0-3 use header string
+var replaceHeader = (file, header) => {
+  if (!header) return
+  const headerArr = header.split('\\n')
+
+  let tmpContent = []
+  const data = fse.readFileSync(file, 'utf8')
+  tmpContent = headerArr.concat(data.split('\n').slice(3))
+  fse.outputFileSync(file, tmpContent.join('\n'))
+  console.log('Replace file header')
+
+  // async
+  // fse.readFile(file, 'utf8').then(data => {
+  //   tmpContent = data.split('\n')
+
+  //   header.toString()
+  //   const headerArr = header.split('\\n')
+
+  //   tmpContent = headerArr.concat(tmpContent.slice(3))
+
+  //   fse.outputFile(file, tmpContent.join('\n')).then(() => {
+  //     console.log('replace file header!')
+  //   })
+  // })
+}
+
 // use standar-version create changelog
 function version() {
   // Options are the same as command line, except camelCase
   // add --noskip args to ignore skip set
   var skip = cmdParser.argv['noskip'] ? {} : { commit: true, tag: true }
+
+  // console.log(cmdParser.argv)
 
   standardVersion(
     {
@@ -119,24 +175,26 @@ function version() {
       skip,
       scripts: {
         postbump: jsonCMD,
-        postchangelog: replaceCMD
-      }
+        postchangelog: replaceCMD,
+      },
     },
     function(err) {
       if (err) {
         console.error(`standard-version failed with message: ${err.message}`)
       }
-      // standard-version is done
     }
-  )
+  ).then(() => {
+    replaceHeader(LOGFILE, HEADER)
+    copyFile(LOGFILE, COPYTO)
+  })
 }
 
 // if --first-release or miss
-if (!fs.existsSync(LOGFILE) || cmdParser.argv['first-release']) {
-  createFolder(LOGFILE)
-  fs.writeFileSync(LOGFILE, '', 'utf8')
-  createLog(LOGFILE)
-}
+// if (!fs.existsSync(LOGFILE) || cmdParser.argv['first-release']) {
+//   createFolder(LOGFILE)
+//   fs.writeFileSync(LOGFILE, '', 'utf8')
+//   createLog(LOGFILE)
+// }
 
 // __main__
 version()
